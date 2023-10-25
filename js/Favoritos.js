3e9c3b460dad319c81499ad4347bd023b6386d20
Nikolas Marlon <40303291+nikolasmarlon@users.noreply.github.com>
2023-10-25 13:23:10 -0300

    Classe conexão com github

--- js/Favoritos.js
+++ js/Favoritos.js
@@ -1,5 +1,20 @@
-// classe que vai conter a lógica dos dados
 
+export class GithubUser {
+    static pesquisar(usuario){
+        const endpoint = `https:api.github.com/users/${usuario}`
+
+
+        // fetch busca as coisas na internet ( promise )  
+        return fetch(endpoint).then( dado  => dado.json()).then( ({ login, name, public_repos, followers}) => ({                 
+            login,
+            name,
+            public_repos,
+            followers
+        }))
+    }
+}
+
+// classe que vai conter a lógica dos dados
 // classe: como os dados serão estruturados
 export class Favoritos{
     constructor(root){
